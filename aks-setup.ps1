db518e6a4ac3658115c56a630c4e8f3f7d852054
Aiden Dipple <aiden.dipple@wetware.com.au>
2023-01-26 15:14:22 +1000

    try again

--- aks-setup.ps1
+++ aks-setup.ps1
@@ -5,8 +5,8 @@ param(
   )
 
   # $output = 'Hello {0}. The username is {1}, the password is {2}.' -f $vaultName,${Env:UserName},${Env:Password}
-  $output = 'ssh to {0}. The username is {1}, the password is {2}.' -f ${Env:UserName},${Env:UserName},${Env:Password}
-  Write-Output $output
+  # $output = 'ssh to {0}. The username is {1}, the password is {2}.' -f ${Env:UserName},${Env:UserName},${Env:Password}
+  # Write-Output $output
 
   # On bootstrapping VM, login using its user assigned identity
   # az login --identity -u /subscriptions/c9c8ae57-acdb-48a9-99f8-d57704f18dee/resourceGroups/avama2-mrg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/avama2mi1
@@ -16,7 +16,7 @@ param(
 
   pwd
   
-  cd '/home/' + ${Env:UserName} + '/.ssh/authorized_keys'
+  cd /home/${Env:UserName}/.ssh/authorized_keys
 
   pwd
   

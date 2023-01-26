c3b823d2fafeddb2292fbf3b265e4df6a15597df
Aiden Dipple <aiden.dipple@wetware.com.au>
2023-01-27 07:23:53 +1000

    test

--- aks-setup.ps1
+++ aks-setup.ps1
@@ -5,8 +5,8 @@ param(
   )
 
   # $output = 'Hello {0}. The username is {1}, the password is {2}.' -f $vaultName,${Env:UserName},${Env:Password}
-  # $output = 'ssh to {0}. The username is {1}, the password is {2}.' -f ${Env:UserName},${Env:UserName},${Env:Password}
-  # Write-Output $output
+  $output = 'ssh to {0}. The username is {1}, the password is {2}.' -f ${Env:UserName},${Env:UserName},${Env:Password}
+  Write-Output $output
 
   # On bootstrapping VM, login using its user assigned identity
   # az login --identity -u /subscriptions/c9c8ae57-acdb-48a9-99f8-d57704f18dee/resourceGroups/avama2-mrg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/avama2mi1
@@ -15,21 +15,13 @@ param(
   # az vmss identity assign -g <VMSS_RESOURCE_GROUP> -n <VMSS_NAME> --identities /subscriptions/c9c8ae57-acdb-48a9-99f8-d57704f18dee/resourceGroups/avama2-mrg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/avama2mi1
 
   
-  ls
-
-
-
-  # pwd
-  
-  # cd /home/${Env:UserName}/.ssh/authorized_keys
-
   # pwd
 
   # ls
   
-  ssh -t ${Env:UserName}@${Env:PublicIpAddress} -tt -i ./deployment-key.txt
+  ssh -tt -i ./deployment-key.txt -t ${Env:UserName}@${Env:PublicIpAddress}
 
-  ls
+  ls -la
   
   pwd
 

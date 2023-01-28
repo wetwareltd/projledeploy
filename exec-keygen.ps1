8dad2bcbf3600c1780534c959bf8161aefb9bd42
Aiden Dipple <aiden.dipple@wetware.com.au>
2023-01-29 08:44:05 +1000

    test

--- exec-keygen.ps1
+++ exec-keygen.ps1
@@ -14,7 +14,7 @@
   $EncodedText = [Convert]::ToBase64String($Bytes)
   
   echo "We have base64 encoded"
-  cat ~/.ssh/id_rsa
+  cat $EncodedText
 
   $DeploymentScriptOutputs = @{}
   $DeploymentScriptOutputs['publicKey'] = cat ~/.ssh/id_rsa.pub

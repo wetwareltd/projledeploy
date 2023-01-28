9d01eeafe6f63beba397d5261068f6c7e668bfa4
Aiden Dipple <aiden.dipple@wetware.com.au>
2023-01-28 14:59:01 +1000

    test

--- aks-setup.ps1
+++ aks-setup.ps1
@@ -21,9 +21,9 @@ param(
   # Assign identity to the VMSS
   # az vmss identity assign -g <VMSS_RESOURCE_GROUP> -n <VMSS_NAME> --identities /subscriptions/c9c8ae57-acdb-48a9-99f8-d57704f18dee/resourceGroups/avama2-mrg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/avama2mi1
 
-  cat $privateKey | id_rsa
+  cat $privateKey > id_rsa.txt
 
-  ssh -tt -i ./id_rsa -t ${Env:UserName}@${Env:PublicIpAddress}
+  ssh -tt -i ./id_rsa.txt -t ${Env:UserName}@${Env:PublicIpAddress}
 
   # curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -H Metadata:true
 

eb01bedb7706ef5fd46b57a770406098c6006b24
Aiden Dipple <aiden.dipple@wetware.com.au>
2023-01-29 09:39:59 +1000

    test

--- aks-setup.ps1
+++ aks-setup.ps1
@@ -2,17 +2,11 @@ param(
     [string] [Parameter(Mandatory=$true)] $privateKey
   )
 
-  echo "privateKey?"
-  
-  echo $privateKey
-
   $encodedText = [Convert]::FromBase64String($privateKey)
   $decodedPrivateKey = [System.Text.Encoding]::UTF8.GetString($encodedText)
   
-  echo "decoded privateKey?"
-  
+  echo "decoded privateKey?"  
   echo $decodedPrivateKey
-
 
   
   # On bootstrapping VM, login using its user assigned identity
@@ -21,9 +15,17 @@ param(
   # Assign identity to the VMSS
   # az vmss identity assign -g <VMSS_RESOURCE_GROUP> -n <VMSS_NAME> --identities /subscriptions/c9c8ae57-acdb-48a9-99f8-d57704f18dee/resourceGroups/avama2-mrg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/avama2mi1
 
+  echo "Create the key file"
   cat $decodedPrivateKey > id_rsa.txt
 
+  
+  echo "Verify the key file"
+  cat id_rsa.txt
+
+  echo "Log in to VM"
   ssh -tt -i ./id_rsa.txt -t ${Env:UserName}@${Env:PublicIpAddress}
+
+  echo "Test local FS"
 
   pwd
 
